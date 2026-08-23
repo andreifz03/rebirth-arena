893451f7a3a513ad5b8c7edcdd2733baade5c3cd
andreifz03 <firescuandrei1@gmail.com>
2026-08-23 15:50:53 +0100

    Rebirth Arena v1.1.0 BIG UPDATE

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('rebirthDesktop', {
-  version: '1.0.5',
+  version: '1.1.0',
   platform: process.platform,
   getAgentArt: () => ipcRenderer.invoke('get-agent-art'),
   checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
